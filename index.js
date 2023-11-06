bfce8f8e5090dcdfb6a0284e8ece3ca3590fa06d
Paulina <paulinasfielek@gmail.com>
2023-11-06 10:00:59 -0800

    changed the code to match instructions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Steps: 
 1. Declare variable for message to user (string)
-CODE = 116
+CODE = 10 - 40 - 39
 2. Declare variable for code 1 (division)
 3. Declare variable for code 2 (subtraction)
 4. Declare variable for code 3 (multiplication)
@@ -10,14 +10,14 @@ CODE = 116
 //variable for message to user (string)//
 let msg = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "; 
 
-//variable for code 1 (division) = 1//
-let code1 = 9/9;
+//variable for code 1 (division) = 10/
+let code1 = 100/10;
 
-//variable for code 2 (subtraction) = 1 //
-let code2 = 8-7;
+//variable for code 2 (subtraction) = 40//
+let code2 = 45-5; 
 
-//variable for code 3 (multiplication) = 6//
-let code3= 2*3;
+//variable for code 3 (multiplication) = 39 //
+let code3= 13*3; 
 
 //final code should = 116//
-alert (msg + code1 + code2 + code3);
+alert (msg + code1 + " - " + code2 + " - " + code3);
