061b24fba8a482d519ef3f21d6a5aa4538b45e5d
Paulina <paulinasfielek@gmail.com>
2023-11-06 09:48:31 -0800

    added HTML & JS

---
+++ index.js
@@ -0,0 +1,50 @@
+/* Guided practice:  
+1. Open the tap
+2. Wet your toothbrush
+3. Open the toothpaste
+4. Put toothpaste on your toothbrush
+5. Wet your toothbrush
+6. Use your toothbrush to brush your teeth for 2 minutes
+7. Spit out the toothpaste 
+8. Rinse off your toothbrush
+9. Put everything away and close the tap
+
+let numCows = 50; 
+let hello = "Hello World!"; 
+console.log (hello); 
+console.log ("Hello " + "Disneyland"); 
+let numUmbrellas = 10 * 3;
+console.log (numUmbrellas);
+let num = 5 + 1; 
+console.log (num);
+const helloWorld = "Hello" + " " + "World";
+console.log (helloWorld);
+let hello2 = "Hello";
+let iam = "I am";
+let name1 = "Josh";
+console.log (hello2 + " " + iam + " " + name1);
+alert (hello2 + " " + iam + " " + name1); */
+
+/* Steps: 
+1. Declare variable for message to user (string)
+CODE = 116
+2. Declare variable for code 1 (division)
+3. Declare variable for code 2 (subtraction)
+4. Declare variable for code 3 (multiplication)
+5. Create alert
+*/ 
+
+//variable for message to user (string)//
+let msg = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "; 
+
+//variable for code 1 (division) = 1//
+let code1 = 9/9;
+
+//variable for code 2 (subtraction) = 1 //
+let code2 = 8-7;
+
+//variable for code 3 (multiplication) = 6//
+let code3= 2*3;
+
+//final code should = 116//
+alert (msg + code1 + code2 + code3);
